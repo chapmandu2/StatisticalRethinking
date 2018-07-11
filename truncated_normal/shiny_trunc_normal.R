8cd21298ce2de5e8edaf24de0f4119d9ce873b24
Phil Chapman <chapmandu2@gmail.com>
2018-07-11 15:34:34 +0000

    Add basic shiny app

---
+++ truncated_normal/shiny_trunc_normal.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+load('~/StatisticalRethinking/truncated_normal/bayesian_trunc_sim_output/sim_output.RData')
+
+make_plot <- function(data, y_val='b_Intercept') {
+    data %>%
+        dplyr::filter(g1 < 600) %>%
+        mutate(g1=as.factor(g1)) %>%
+        ggplot(aes_(color=~g1, y=as.name(y_val), x=~g1)) + 
+        geom_boxplot() + 
+        facet_grid(as.factor(d)~model) +
+        theme_bw()
+}
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Bayesian Simulation Results"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         selectInput("selected_yval",
+                     "Value to plot on y-axis:",
+                     choices = c('b_Intercept', 'b_p1', 'diff_intercept', 'diff_p1'))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("plot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$plot <- renderPlot({
+       make_plot(result_df, input$selected_yval)
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
