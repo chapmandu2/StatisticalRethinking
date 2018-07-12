3f54af97786a4eb5220858abca5fc2574f9fc684
Phil Chapman <chapmandu2@gmail.com>
2018-07-12 11:20:41 +0000

    Add bayesian estimation to shiny_trunc_sim

--- truncated_normal/shiny_trunc_sim.R
+++ truncated_normal/shiny_trunc_sim.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(brms)
+library(broom)
 
-load('~/StatisticalRethinking/truncated_normal/bayesian_trunc_sim_output/sim_output.RData')
+load('~/StatisticalRethinking/truncated_normal/bayesian_trunc_sim_output/brms_models.RData')
 
 
 make_data <- function(d=50, ub=600, g1=500, sd=80, n=1000) {
@@ -21,12 +23,12 @@ make_plot <- function(data) {
     true_diff <- lm(data ~ p1, data=data)$coef[[2]]
     
     data %>%
-        select(p1, data, cens_data, trunc_data) %>%
+        select(p1, data, cens_data) %>%
         tidyr::gather('dataset', 'value', -p1) %>%
         filter(!is.na(value) ) %>%
         mutate(p1 = as.factor(p1)) %>%
         ggplot() + 
-        geom_histogram(aes(x=value, fill=p1), bins=50) +
+        geom_histogram(aes(x=value, fill=p1), bins=50) + xlim(0,1000) +
         geom_vline(xintercept = true_g1, colour='red', linetype='dotted') + 
         geom_vline(xintercept = true_g1 + true_diff, colour='green', linetype='dotted') + 
         facet_grid(p1~dataset, scales = 'free_y') + 
@@ -57,7 +59,12 @@ ui <- fluidPage(
         
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("plot")
+            plotOutput("plot"),
+            helpText('Simulated Data'),
+            tableOutput('lm_out'),
+            helpText('Censored Data via Bayesian Estimation'),
+            tableOutput('brms_out'),
+            plotOutput('stanplot')
         )
     )
 )
@@ -69,8 +76,28 @@ server <- function(input, output) {
         make_data(d=input$selected_d, ub=600, g1=input$selected_g1, sd=input$selected_sd, n=1000)
     })
     
+    brms_fit <- reactive({
+        update(brms_mod_cens, newdata=plot_dat(), recompile=FALSE, 
+               chains=4, iter=1000, init_r=5, cores=4)
+    })
+    
+    brms_fit_coefs <- reactive({
+        broom::tidy(brms_fit())
+    })
+    
     output$plot <- renderPlot({
         make_plot(plot_dat())
+    })
+    
+    output$brms_out <- renderTable(brms_fit_coefs())
+    
+    output$lm_out <- renderTable({
+        lm(data ~ p1, data=plot_dat()) %>% broom::tidy()}
+        )
+    
+    output$stanplot <- renderPlot({
+        brms::stanplot(brms_fit(), type='trace') + 
+            facet_wrap(~Parameter, ncol=1, scales = 'free_y')
     })
 }
 
